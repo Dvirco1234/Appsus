2a5b41e806ec2d4bc195b21e8bc9bffdbf5da02b
Dvir Cohen <dvirco123@gmail.com>
2022-06-29 11:29:36 +0300

    basic setup1

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -5,9 +5,9 @@ export default {
         <router-link to="/"><h3>appsus</h3></router-link>
       </div>
       <nav class="">
-        <router-link to="/">Home</router-link>
-        <router-link to="/book">Books</router-link>
-        <router-link to="/mail">mail</router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/book">Books</router-link> |
+        <router-link to="/mail">mail</router-link> |
         <router-link to="/keep">keep</router-link>
       </nav>
     </header>
--- js/router.js
+++ js/router.js
@@ -1,9 +1,14 @@
+import keepApp from './apps/keep/pages/keep-app.cmp.js'
 import homePage from './pages/home-page.cmp.js'
 
 const routes = [
     {
         path: '/',
         component: homePage,
+    },
+    {
+        path: '/keep',
+        component: keepApp,
     },
 ]
 
