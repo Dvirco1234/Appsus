f31b6bb2baa031cedfee6ff3d54e8e33949e712e
Dvir Cohen <dvirco123@gmail.com>
2022-06-29 10:46:36 +0300

    basic setup

---
+++ js/cmps/app-header.cmp.js
@@ -0,0 +1,20 @@
+export default {
+    template: `
+    <header class="app-header">
+    <div class="logo">
+        <router-link to="/"><h3>appsus</h3></router-link>
+      </div>
+      <nav class="">
+        <router-link to="/">Home</router-link>
+        <router-link to="/book">Books</router-link>
+        <router-link to="/mail">mail</router-link>
+        <router-link to="/keep">keep</router-link>
+      </nav>
+    </header>
+    `,
+    data() {
+        return {}
+    },
+    methods: {},
+    computed: {},
+}
